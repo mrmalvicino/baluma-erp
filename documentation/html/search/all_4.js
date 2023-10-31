b539b827414f5dcb96a9197e564f5791f9a3dcb8
mrmalvicino <mrmalvicino@gmail.com>
2023-10-31 12:13:08 -0300

    Baluma ERP v1.0.0 documentation

--- documentation/html/search/all_4.js
+++ documentation/html/search/all_4.js
@@ -4,8 +4,9 @@ var searchData=
   ['baluma_20erp_1',['baluma erp',['../md__2_users_2mrmalvicino_2_desktop_2_git_hub_2baluma-erp_2_r_e_a_d_m_e.html',1,'Baluma ERP'],['../md__2_users_2mrmalvicino_2_desktop_2_git_hub_2baluma-erp_2_r_e_a_d_m_e-_e_s_p.html',1,'Baluma ERP']]],
   ['baluma_2ecpp_2',['Baluma.cpp',['../_baluma_8cpp.html',1,'']]],
   ['baluma_2eh_3',['Baluma.h',['../_baluma_8h.html',1,'']]],
-  ['businesspartner_4',['businesspartner',['../class_business_partner.html',1,'BusinessPartner'],['../class_business_partner.html#a795e7114219e2afc8c26503af56827cf',1,'BusinessPartner::BusinessPartner()'],['../class_business_partner.html#a2769b034a1439e32c765a71b5ba45f1f',1,'BusinessPartner::BusinessPartner(int id, const std::string &amp;description, long long int legal_id, const Adress &amp;adress, int phone, const std::string &amp;email, bool is_active)']]],
+  ['businesspartner_4',['businesspartner',['../class_business_partner.html#a2769b034a1439e32c765a71b5ba45f1f',1,'BusinessPartner::BusinessPartner()'],['../class_business_partner.html',1,'BusinessPartner'],['../class_business_partner.html#a795e7114219e2afc8c26503af56827cf',1,'BusinessPartner::BusinessPartner()']]],
   ['businesspartner_2ecpp_5',['BusinessPartner.cpp',['../_business_partner_8cpp.html',1,'']]],
   ['businesspartner_2eh_6',['BusinessPartner.h',['../_business_partner_8h.html',1,'']]],
-  ['buy_7',['buy',['../class_accounting_manager.html#ac48f35f6fa958282590b89ba0aa6c4ec',1,'AccountingManager']]]
+  ['businesspartnersmenu_7',['businessPartnersMenu',['../class_baluma.html#a2ad20f5bdd4b8296f727b7e93c55d523',1,'Baluma']]],
+  ['buy_8',['buy',['../class_accounting_manager.html#ac48f35f6fa958282590b89ba0aa6c4ec',1,'AccountingManager']]]
 ];
--- documentation/html/search/functions_1.js
+++ documentation/html/search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['businesspartner_0',['businesspartner',['../class_business_partner.html#a795e7114219e2afc8c26503af56827cf',1,'BusinessPartner::BusinessPartner()'],['../class_business_partner.html#a2769b034a1439e32c765a71b5ba45f1f',1,'BusinessPartner::BusinessPartner(int id, const std::string &amp;description, long long int legal_id, const Adress &amp;adress, int phone, const std::string &amp;email, bool is_active)']]],
-  ['buy_1',['buy',['../class_accounting_manager.html#ac48f35f6fa958282590b89ba0aa6c4ec',1,'AccountingManager']]]
+  ['businesspartnersmenu_1',['businessPartnersMenu',['../class_baluma.html#a2ad20f5bdd4b8296f727b7e93c55d523',1,'Baluma']]],
+  ['buy_2',['buy',['../class_accounting_manager.html#ac48f35f6fa958282590b89ba0aa6c4ec',1,'AccountingManager']]]
 ];
